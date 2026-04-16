338462ce292687ce5ee95205ec3e259c3e6deb19
Frank Obiaya <obiayafc@gmail.com>
2026-04-16 13:27:32 -0400

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.kt
@@ -0,0 +1,28 @@
+class Solution {
+    fun checkInclusion(s1: String, s2: String): Boolean {
+        if (s2.length < s1.length) return false
+
+        val s1Count = IntArray(26)
+        val windowCount = IntArray(26)
+
+        // Populate frequency arrays for s1 and the first window of s2
+        for (i in s1.indices) {
+            s1Count[s1[i] - 'a']++
+            windowCount[s2[i] - 'a']++
+        }
+
+        if (s1Count.contentEquals(windowCount)) return true
+        var start = 0
+
+        // Slide the window: bring in the new right char, drop the old left char
+        for (end in s1.length until s2.length) {
+            //val start = end - s1.length
+            println("start $start")
+            windowCount[s2[end] - 'a']++
+            windowCount[s2[start++] - 'a']--
+            if (s1Count.contentEquals(windowCount)) return true
+        }
+
+        return false
+    }
+}
